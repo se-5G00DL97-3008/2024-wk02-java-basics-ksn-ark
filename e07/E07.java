e477e67fb6f3abf0191c7510d4dde320687a8118
Nark <sooryarox2004@gmail.com>
2024-01-18 01:30:09 +0200

    Update E07.java

--- e07/E07.java
+++ e07/E07.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 class E07 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which multiplication table to show?");
+        int a = scanner.nextInt();
+        System.out.println("Until which multiplier should the table show?");
+        int b = scanner.nextInt();
+        scanner.close();
+        for (int i = 0; i <= b; i++) {
+            System.out.printf("%d x %d = %d%n", i, a, a * i);
+        }
     }
 }
