ddf25644ffc772c0e67e2003325866590de3dbf7
Nark <sooryarox2004@gmail.com>
2024-01-18 01:23:26 +0200

    Update E05.java

--- e05/E05.java
+++ e05/E05.java
@@ -3,30 +3,30 @@ import java.util.Scanner;
 class E05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter a number: ");
+        System.out.println("Enter a number:");
         int day = scanner.nextInt();
         scanner.close();
         switch (day) {
             case 1:
-                System.out.println("Mon");
+                System.out.println("Monday");
                 break;
             case 2:
-                System.out.println("Tue");
+                System.out.println("Tueday");
                 break;
             case 3:
-                System.out.println("Wed");
+                System.out.println("Wedday");
                 break;
             case 4:
-                System.out.println("Thu");
+                System.out.println("Thursday");
                 break;
             case 5:
-                System.out.println("Fri");
+                System.out.println("Friday");
                 break;
             case 6:
-                System.out.println("Sat");
+                System.out.println("Saturday");
                 break;
             case 7:
-                System.out.println("Sun");
+                System.out.println("Sunday");
                 break;
             default:
                 System.out.println("Only 7 days in a week");
