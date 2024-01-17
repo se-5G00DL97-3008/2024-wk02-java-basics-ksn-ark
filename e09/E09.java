a1b0f04c01c34c19c45f94521c615ded6e72f69e
Nark <sooryarox2004@gmail.com>
2024-01-18 01:40:01 +0200

    Update E09.java

--- e09/E09.java
+++ e09/E09.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 class E09 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+        int a = 0;
+        while (true) {
 
+            System.out.println("Give a test score (-1 to quit):");
+            int b = scanner.nextInt();
+            if (b == -1) {
+                break;
+            }
+            a += b;
+            i += 1;
+            System.out.println("Average: " + a / i);
+        }
+        scanner.close();
     }
 }
