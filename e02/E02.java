dbbc01193559f497d74b1445a3495e6ced574ed4
Nark <sooryarox2004@gmail.com>
2024-01-18 00:40:39 +0200

    Update E02.java

--- e02/E02.java
+++ e02/E02.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 class E02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Give a number:");
-        int a = Integer.parseInt(scanner.nextLine());
-        System.out.println("Give a number:");
-        int b = Integer.parseInt(scanner.nextLine());
+        System.out.println("Give a number: ");
+        int a = scanner.nextInt();
+        System.out.println("Give a number: ");
+        int b = scanner.nextInt();
         System.out.printf("%d + %d = %d%n", a, b, a + b);
         System.out.printf("%d - %d = %d%n", a, b, a - b);
         System.out.printf("%d x %d = %d%n", a, b, a * b);
