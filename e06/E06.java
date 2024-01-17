e345b9c4de8f458e28fea0f2a9af64dcd70aa150
Nark <sooryarox2004@gmail.com>
2024-01-18 01:27:05 +0200

    Update E06.java

--- e06/E06.java
+++ e06/E06.java
@@ -6,7 +6,7 @@ class E06 {
         System.out.println("Which multiplication table to show?");
         int a = scanner.nextInt();
         scanner.close();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 11; i++) {
             System.out.printf("%d x %d = %d%n", a, i, a * i);
         }
     }
