cc782001851542e43945ad8af6c1f459567fc57d
Nark <sooryarox2004@gmail.com>
2024-01-18 01:33:46 +0200

    Update E08.java

--- e08/E08.java
+++ e08/E08.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 class E08 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
 
+            System.out.println("Give a number (0 to quit):");
+            int a = scanner.nextInt();
+            if (a == 0) {
+                break;
+            }
+            if (a % 2 == 0) {
+                System.out.println("Even number");
+            } else {
+                System.out.println("Odd number");
+            }
+        }
+        scanner.close();
     }
 }
