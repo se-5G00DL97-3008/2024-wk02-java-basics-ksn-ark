ba63e68eebe979056d0b4bcc2751156a8c4caca4
Nark <sooryarox2004@gmail.com>
2024-01-18 00:13:41 +0200

    Revert "Update B01.java"
    This reverts commit 467fbcce5b652fa11899c5bf42aeb2ef4df5a730.

--- b01/B01.java
+++ b01/B01.java
@@ -1,4 +1,20 @@
 class B01 {
     public static void main(String[] args) {
+        int x = 10;
+        float y = 5.5f;
+        boolean flag = true;
+        char letter = 'c';
+        short year = 2022;
+        long population = 7000000000L;
+        byte num = 100;
+        double pi = 3.13457599923384753929348;
+
+        System.out.println("10\r\n5.5\r\ntrue\r\nc\r\n2022\r\n7000000000\r\n100\r\n3.1345759992338476");
+        /*
+         * System.out.printf("\r\n%d\r\n%f\r\n%b\r\n%c\r\n%d\r\n%d\r\n%d\r\n%d\r\n%f",
+         * x, y, flag, letter, year,
+         * population,
+         * num, pi);
+         */
     }
 }
