a233c0c2e8a642eef31fcae74d09639a6f816cae
Nark <sooryarox2004@gmail.com>
2024-01-18 00:38:53 +0200

    Update E02.java

--- e02/E02.java
+++ e02/E02.java
@@ -4,9 +4,9 @@ class E02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Give a number:");
-        int a = scanner.nextLine();
+        int a = Integer.parseInt(scanner.nextLine());
         System.out.println("Give a number:");
-        int b = scanner.nextLine();
+        int b = Integer.parseInt(scanner.nextLine());
         System.out.printf("%d + %d = %d%n", a, b, a + b);
         System.out.printf("%d - %d = %d%n", a, b, a - b);
         System.out.printf("%d x %d = %d%n", a, b, a * b);
