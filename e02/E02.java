99c1e2a14da22c63a138f9a998a1350b113c9a96
Nark <sooryarox2004@gmail.com>
2024-01-18 00:23:10 +0200

    Update E02.java

--- e02/E02.java
+++ e02/E02.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 class E02 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give a number:");
+        int a = scanner.nextInt();
+        System.out.println("Give another number:");
+        int b = scanner.nextInt();
+        scanner.close();
+        System.out.printf("%d\r\n", a + b);
+        System.out.printf("%d\r\n", a + b);
+        System.out.printf("%d\r\n", a + b);
+        System.out.printf("%d\r\n", a + b);
     }
 }
